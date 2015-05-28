2a429592350e188c64e415628ea42901b0fb894b
Jefferson Choi <jeffersonchoi@gmail.com>
2015-05-28 14:49:16 -0700

    added orderline model and updated relationship

---
+++ db/migrate/20150528214633_create_orderlines.rb
@@ -0,0 +1,13 @@
+class CreateOrderlines < ActiveRecord::Migration
+  def change
+    create_table :orderlines do |t|
+      t.references :item
+      t.references :order
+      t.integer :quantity
+      t.float :selling_price
+      t.float :total_price
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528213418) do
+ActiveRecord::Schema.define(version: 20150528214633) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "user_id"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20150528213418) do
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
+
+  create_table "orderlines", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "order_id"
+    t.integer  "quantity"
+    t.float    "selling_price"
+    t.float    "total_price"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
